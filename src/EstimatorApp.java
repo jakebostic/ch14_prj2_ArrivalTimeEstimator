bced6ffb11959d3360bb3dc41f3e87f7b1d7e7a9
jakebostic <jake_bostic@yahoo.com>
2018-06-20 13:07:42 -0400

    Initial Commit

---
+++ src/EstimatorApp.java
@@ -0,0 +1,19 @@
+
+public class EstimatorApp {
+
+	public static void main(String[] args) {
+		System.out.println("Arrival Time Estimator");
+		System.out.println();
+		
+		String choice = "y";
+		while (choice.equalsIgnoreCase("y")) {
+			System.out.println("Departure date (YYYY-MM-DD): ");
+			
+		}
+		
+		
+		System.out.println("Bye!");
+
+	}
+
+}
